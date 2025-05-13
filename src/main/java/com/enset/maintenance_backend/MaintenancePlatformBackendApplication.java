3415ae3abe993e55c9e85a0cb202179f829b3db6
Nisrine-C <nisrinechakir237@gmail.com>
2025-05-13 19:03:18 +0100

    Initial Commit

---
+++ src/main/java/com/enset/maintenance_backend/MaintenancePlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enset.maintenance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenancePlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenancePlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enset/maintenance_backend/MaintenancePlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enset.maintenance_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenancePlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
